0ffcdcded28c9cab76d947c2198394e5f746c48b
Denys Ferreira <denys@deckdesign.com.br>
2021-11-17 17:51:51 -0300

    [ADD]Adicionado condição para quando não houver genero e atores.

--- src/screens/Filmes/Detalhes.jsx
+++ src/screens/Filmes/Detalhes.jsx
@@ -75,14 +75,25 @@ const Detalhes = ({navigation, route}) => {
                             <List.Accordion
                                 title="Gêneros"
                                 left={props => <List.Icon {...props} icon="movie-filter" />}>
-                                {filme.genres.map(genero => (
+                                {filme.genres.length <= 0 &&
+                                <View>
+                                    <List.Item
+                                        title="Gênero indisponível"
+                                        description="Gênero indisponível"
+                                    />
+                                </View>
+                                }
+                                {filme.genres.length > 0 &&
+                                filme.genres.map(genero => (
                                     <View key={genero.id}>
                                         <TouchableOpacity
                                             onPress={() => navigation.push('filmes/genero', { id: genero.id, name: genero.name })}>
                                             <List.Item title={genero.name} />
                                         </TouchableOpacity>
                                     </View>
-                                ))}
+                                ))
+
+                                }
                             </List.Accordion>
                         </Col>
                     </Row>
@@ -91,19 +102,28 @@ const Detalhes = ({navigation, route}) => {
                             <List.Accordion
                                 title="Atores"
                                 left={props => <List.Icon {...props} icon="account-multiple" />}>
-                                {atores.map(ator => (
-                                    <View key={ator.id}>
-                                        <TouchableOpacity
-                                                          onPress={() => navigation.push('atores/detalhes', { id: ator.id })}>
-                                            <List.Item
-                                                title={ator.character}
-                                                description={ator.name}
-                                                left={() => imagemAtor(ator.profile_path)}
-                                            />
-                                        </TouchableOpacity>
-                                    </View>
-
-                                ))}
+                                {atores.length <= 0 &&
+                                <View>
+                                    <List.Item
+                                        title="Nenhum ator encontrado."
+                                        description="Nenhum ator encontrado."
+                                    />
+                                </View>
+                                }
+                                {atores.length > 0 &&
+                                    atores.map(ator => (
+                                        <View key={ator.id}>
+                                            <TouchableOpacity
+                                                              onPress={() => navigation.push('atores/detalhes', { id: ator.id })}>
+                                                <List.Item
+                                                    title={ator.character}
+                                                    description={ator.name}
+                                                    left={() => imagemAtor(ator.profile_path)}
+                                                />
+                                            </TouchableOpacity>
+                                        </View>
+                                    ))
+                                }
                             </List.Accordion>
                         </Col>
                     </Row>
